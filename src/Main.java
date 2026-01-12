06e4355db36993c8239fc5b2064ac34106ed8d0b
jinho <jinhoftyu@naver.com>
2026-01-12 13:55:18 +0900

    Hello, World!

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,9 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        char[] p = s.toCharArray();
-        for (int i = 0; i < p.length; i++) {
-            char c = p[i];
-            if (c == '*' || c == '/') {
-                s = s.substring(0, i - 1) + "(" + s.substring(i - 1, i + 2) + ")" + s.substring(i + 2);
-            }
-        }
-        System.out.println(s);
-//        for (char c : p) {
-//            if (c == '(') {
-//                s.push(c);
-//            } else {
-//                if (s.isEmpty()) {
-//
-//                } else {
-//                    s.pop();
-//                }
-//            }
-//        }
-
+        System.out.println("Hello, World!");
     }
+
 }
---
+++ src/archive/Silver/DP/S11053X1.java
@@ -0,0 +1,40 @@
+package archive.Silver.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class S11053X1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = Stream.of(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int opt = 0;
+        for (int k = 1; k < n; k++) {
+            int[] dp = new int[n];
+            int bef = 0;
+            int max = arr[k - 1];
+            dp[k - 1] = 1;
+            for (int i = k; i < n; i++) {
+                int cur = arr[i];
+                if (cur > max) {
+                    dp[i] = dp[i - 1] + 1;
+                    bef = max;
+                    max = cur;
+                } else {
+                    dp[i] = dp[i - 1];
+                    if (cur > bef) {
+                        max = cur;
+                    }
+                }
+            }
+            if (dp[n - 1] > opt) {
+                opt = dp[n - 1];
+            }
+        }
+        System.out.println(opt);
+    }
+
+}
